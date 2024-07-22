3499cb47c1a35ba38afbc7c163a69c987f8642a3
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2024-07-22 22:51:34 +0200

    v2

--- src/app/page.js
+++ src/app/page.js
@@ -2,66 +2,78 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-screen text-white px-[128px] bg-[#151515]">
-      <section className="h-screen flex justify-left items-center overflow-hidden">
-        <div className="w-full pt-[24px] absolute top-0 left-0 z-10 px-[128px] flex items-center justify-between">
-          <p className="text-[32px] text-white font-semibold">TaxiBilen</p>
-          {/* <button className="bg-white text-black px-[32px] py-[8px] rounded-full font-medium">
+    <main className="relative text-white px-[32px] lg:px-[128px] bg-[#101010]">
+      <div className="absolute inset-0 z-0 h-[80vh]">
+        <Image
+          src="/road2.jfif"
+          alt="Image"
+          layout="fill"
+          objectFit="cover"
+          style={{ transform: "scaleX(-1)", objectPosition: "right" }}
+        />
+        {/* <div
+          style={{
+            position: "absolute",
+            inset: "0",
+            backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust the rgba value for darkness
+            zIndex: 10,
+          }}
+        ></div> */}
+      </div>
+      <section className="h-[80vh] flex justify-left items-center overflow-hidden">
+        <div className="w-full pt-[24px] absolute top-0 left-0 z-10 flex items-center justify-between px-[32px] lg:px-[128px]">
+          <p className="text-[22px] text-white font-semibold">TaxiBilen</p>
+          {/* <button className="bg-white text-black px-[16px] py-[4px] rounded-full font-medium">
             Registrera Dig
           </button> */}
         </div>
-        <div className="absolute inset-0 z-0 overflow-hidden">
-          {/* <Image
-            src="/heroImg.jpg"
-            alt="Hero Image"
-            layout="fill"
-            objectFit="cover"
-            className="object-top"
-          /> */}
-        </div>
-        <div className="relative h-2/4 flex z-10 flex flex-col justify-start items-start text-left">
-          <p className="text-[96px] font-bold text-white leading-10">
-            Åk vart du vill
+
+        <div className="relative h-2/4 flex z-10 flex flex-col justify-start items-start text-left gap-8 drop-shadow-lg">
+          <p className="text-[48px] lg:text-[64px] lg:w-2/3 font-bold text-white leading-[50px] lg:leading-[70px] drop-shadow-lg">
+            Åk vart du vill med{" "}
+            <span className="text-[#FFE500]">TaxiBilen</span>
           </p>
-          <p className="text-[96px] font-bold text-white">
-            med <span className="text-[#FFE500]">TaxiBilen</span>
+          <p className="text-white text-[22px] lg:text-[32px] lg:w-2/3">
+            Res säkert och bekvämt. Kontakta oss direkt och förboka din taxi i
+            god tid.
           </p>
-          <p className="text-white text-[48px]">
-            Kontakta oss direkt och förboka
-          </p>
-          <p className="text-white text-[48px]">din taxi i god tid</p>
         </div>
       </section>
-      <section className="min-h-screen pt-[64px] relative px-[256px]">
-        <section className="w-full text-center flex flex-col justify-center items-center">
-          <h1 className="text-[64px] font-semibold">
+      <section className="min-h-screen pt-[64px] relative px-[32px] lg:px-[228px] pb-[256px]">
+        <section className="w-full text-center flex flex-col justify-center items-center gap-2 lg:gap-8">
+          <h1 className="text-[36px] lg:text-[48px] font-semibold leading-[50px]">
             Mer än bara en <span className="text-[#FFE500]">taxi</span>
           </h1>
-          <p className="text-[32px] leading-10">
+          <p className="text-[22px] lg:text-[28px] leading-10">
             Vi kör även bud med plats för en låg{" "}
             <span className="text-[#FFE500]">lastpall</span>, ordentligt{" "}
-            <span className="text-[#FFE500]">takräcke</span>
-            för långa saker och möjlighet att köra{" "}
-            <span className="text-[#FFE500]">släp</span> med t.ex. båt
+            <span className="text-[#FFE500]">takräcke</span> för långa saker och
+            möjlighet att köra <span className="text-[#FFE500]">släp</span> med
+            t.ex. båt
           </p>
         </section>
-        <section className="relative flex flex-row justify-between items-center gap-[64px] text-[32px] pt-[128px]">
-          <div className="w-1/2 flex flex-col gap-[16px]">
-            <p className="text-[48px] leading-[50px] font-semibold">
-              Lokal <span className="text-[#FFE500]">Taxi</span> for
+        <section className="relative flex flex-col-reverse lg:flex-row justify-between items-center gap-[64px] text-[22px] lg:text-[28px] pt-[32px] lg:pt-[128px]">
+          <div className="text-center flex flex-col gap-2 lg:gap-8 lg:w-[60%]">
+            <p className="text-[36px] lg:text-[48px] leading-[50px] font-semibold">
+              Lokal <span className="text-[#FFE500]">Taxi</span> för
               lokalbefolkningen
             </p>
-            <p>
-              Vårt <span className="text-[#FFE500]">mål</span> är att finnas här
-              för lokalbefolkningen{" "}
-              <span className="text-[#FFE500]">året om. Fasta priser</span> och
-              enkelt upplägg
-            </p>
+            <div>
+              <p>
+                Vårt <span className="text-[#FFE500]">mål</span> är att finnas
+                här för lokalbefolkningen{" "}
+              </p>
+              <p>
+                året om.
+                <span className="text-[#FFE500]"> Fasta priser</span> och enkelt
+                upplägg
+              </p>
+            </div>
           </div>
-          <div className="relative w-[50%] aspect-square">
+          <div className="relative w-[70%] lg:w-[40%] aspect-square">
             <Image
               className="rounded-[40px]"
-              src="/road.png"
+              src="/taxiBilen.jpg"
               alt="Image"
               layout="fill"
               objectFit="cover"
@@ -69,7 +81,11 @@ export default function Home() {
           </div>
         </section>
       </section>
-      <section className="h-screen"></section>
+      <footer className="bg-[#101010] text-white text-center py-4 absolute bottom-0 inset-x-0 ">
+        <p className="text-[16px]">
+          &copy; {new Date().getFullYear()} Invicom. All rights reserved.
+        </p>
+      </footer>
     </main>
   );
 }
