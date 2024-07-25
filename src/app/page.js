10286170b220ddcd6ab2a7ff587144e9369cdd49
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2024-07-25 19:46:02 +0200

    gradient

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative text-white px-[32px] lg:px-[128px] bg-[#101010]">
+    <main className="relative text-white px-[32px] 2xl:px-[128px] bg-[#101010]">
       <div className="absolute inset-0 z-0 h-[80vh]">
         <Image
           src="/road2.jfif"
@@ -19,32 +19,33 @@ export default function Home() {
             zIndex: 10,
           }}
         ></div> */}
+        <div className="absolute w-full h-1/5 bottom-0 bg-gradient-to-t from-[#101010]"></div>
       </div>
       <section className="h-[80vh] flex justify-left items-center overflow-hidden">
-        <div className="w-full pt-[24px] absolute top-0 left-0 z-10 flex items-center justify-between px-[32px] lg:px-[128px]">
+        <div className="w-full pt-[24px] absolute top-0 left-0 z-10 flex items-center justify-between px-[32px] 2xl:px-[128px]">
           <p className="text-[22px] text-white font-semibold">TaxiBilen</p>
           {/* <button className="bg-white text-black px-[16px] py-[4px] rounded-full font-medium">
             Registrera Dig
           </button> */}
         </div>
 
-        <div className="relative h-2/4 flex z-10 flex flex-col justify-start items-start text-left gap-8 drop-shadow-lg">
-          <p className="text-[48px] lg:text-[64px] lg:w-2/3 font-bold text-white leading-[50px] lg:leading-[70px] drop-shadow-lg">
+        <div className="relative h-2/4 flex z-10 flex flex-col justify-start items-start text-left gap-8 drop-shadow-2xl">
+          <p className="text-[48px] 2xl:text-[64px] 2xl:w-2/3 font-bold text-white leading-[50px] 2xl:leading-[70px] drop-shadow-2xl">
             Åk vart du vill med{" "}
             <span className="text-[#FFE500]">TaxiBilen</span>
           </p>
-          <p className="text-white text-[22px] lg:text-[32px] lg:w-2/3">
+          <p className="text-white text-[22px] 2xl:text-[32px] 2xl:w-2/3">
             Res säkert och bekvämt. Kontakta oss direkt och förboka din taxi i
             god tid.
           </p>
         </div>
       </section>
-      <section className="min-h-screen pt-[64px] relative px-[32px] lg:px-[228px] pb-[256px]">
-        <section className="w-full text-center flex flex-col justify-center items-center gap-2 lg:gap-8">
-          <h1 className="text-[36px] lg:text-[48px] font-semibold leading-[50px]">
+      <section className="min-h-screen pt-[64px] relative px-[32px] 2xl:px-[228px] pb-[256px]">
+        <section className="w-full text-center flex flex-col justify-center items-center gap-2 2xl:gap-8">
+          <h1 className="text-[36px] 2xl:text-[48px] font-bold leading-[50px]">
             Mer än bara en <span className="text-[#FFE500]">taxi</span>
           </h1>
-          <p className="text-[22px] lg:text-[28px] leading-10">
+          <p className="text-[22px] 2xl:text-[28px] leading-10">
             Vi kör även bud med plats för en låg{" "}
             <span className="text-[#FFE500]">lastpall</span>, ordentligt{" "}
             <span className="text-[#FFE500]">takräcke</span> för långa saker och
@@ -52,9 +53,9 @@ export default function Home() {
             t.ex. båt
           </p>
         </section>
-        <section className="relative flex flex-col-reverse lg:flex-row justify-between items-center gap-[64px] text-[22px] lg:text-[28px] pt-[32px] lg:pt-[128px]">
-          <div className="text-center flex flex-col gap-2 lg:gap-8 lg:w-[60%]">
-            <p className="text-[36px] lg:text-[48px] leading-[50px] font-semibold">
+        <section className="relative flex flex-col-reverse 2xl:flex-row justify-between items-center gap-[64px] text-[22px] 2xl:text-[28px] pt-[32px] 2xl:pt-[128px]">
+          <div className="text-center flex flex-col gap-2 2xl:gap-8 2xl:w-[60%]">
+            <p className="text-[36px] 2xl:text-[48px] leading-[50px] font-bold">
               Lokal <span className="text-[#FFE500]">Taxi</span> för
               lokalbefolkningen
             </p>
@@ -70,7 +71,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="relative w-[70%] lg:w-[40%] aspect-square">
+          <div className="relative w-[70%] 2xl:w-[40%] aspect-square">
             <Image
               className="rounded-[40px]"
               src="/taxiBilen.png"
